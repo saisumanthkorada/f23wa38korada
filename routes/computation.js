cebfe647e890d1c737c83c2d292c1b0670aa6064
saisumanthkorada <137339004+saisumanthkorada@users.noreply.github.com>
2023-10-25 20:47:10 -0500

    new endpoint

--- routes/computation.js
+++ routes/computation.js
@@ -1,13 +1,9 @@
 var express = require('express');
 var router = express.Router();
-sum =0;
-more =0;
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  more = more +1;
-  sum = sum + more;
-  res.send('Sum is :'+sum);
+  res.render('computation', { title: 'Using Math functions' });
 });
 
 module.exports = router;
